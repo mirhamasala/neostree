4668c92efe43dccaecb3902c598a04e34f84ce1e
mirhamasala <mirha.masala@me.com>
2020-07-04 18:04:04 +0800

    Create a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,15 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
   end
 
   def show
@@ -18,5 +22,12 @@ class RecipesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :link, :photo,
+      :notes, :prep_time, :cook_time, :servings, :step_id, :measure_id)
   end
 end
