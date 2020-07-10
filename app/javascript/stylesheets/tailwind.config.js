5815d422917aab4e32005ebb6b72afa3606eac3c
mirhamasala <mirha.masala@me.com>
2020-07-10 11:03:22 +0800

    Add colors to Tailwind config

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "sunny-up": "0 0 0 3px rgba(255, 255, 09, 0.2)"
+      },
+      colors: {
+        dark: {
+          "800": "#16181c",
+          "900": "#000000",
+        },
+        "sunny-up": {
+          "100": "#ffffd1",
+          "200": "#ffdf56",
+          "300": "#fecb37",
+          "400": "#f3b938",
+          "500": "#e19739",
+          "600": "#d08539",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
