57702e4d4e63138c59a20cac10026145ab239f5c
mirhamasala <mirha.masala@me.com>
2020-07-11 09:38:45 +0800

    Add redirects for when creating and deleting a measure

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -8,14 +8,17 @@ class MeasuresController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @measure = Measure.new(measure_params)
     @measure.recipe = @recipe
-    @measure.save
-    redirect_to recipe_path(@recipe)
+    if @measure.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
 
   def destroy
     @measure = Measure.find(params[:id])
     @measure.destroy
-    redirect_back(fallback_location:"/")
+    redirect_to recipe_path(@measure.recipe)
   end
 
   private
