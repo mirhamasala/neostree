6e308c91093a506778953a205da50d641826d148
mirhamasala <mirha.masala@me.com>
2020-07-11 09:37:12 +0800

    Add redirects for when creating and deleting a ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,13 +5,17 @@ class IngredientsController < ApplicationController
 
   def create
     @ingredient = Ingredient.new(ingredient_params)
-    @ingredient.save
+    if @ingredient.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
 
   def destroy
     @ingredient = Ingredient.find(params[:id])
     @ingredient.destroy
-    redirect_back(fallback_location: dashboard_path)
+    redirect_to dashboard_path
   end
 
   private
