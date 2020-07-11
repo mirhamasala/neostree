e77cd3aec3e77f7e6e80beb61103e8c66210334d
mirhamasala <mirha.masala@me.com>
2020-07-11 09:42:31 +0800

    Add redirects for when creating and deleting a step

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -8,13 +8,17 @@ class StepsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @step = Step.new(steps_params)
     @step.recipe = @recipe
-    @step.save
-    redirect_to recipe_path(@recipe)
+    if @step.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
 
   def destroy
     @step = Step.find(params[:id])
     @step.destroy
+    redirect_to recipe_path(@step.recipe)
   end
 
   private
