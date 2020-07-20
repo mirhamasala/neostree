008e269fa67d766aa07d2d98a17e2a9b0d45a28e
mirhamasala <mirha.masala@me.com>
2020-07-20 17:20:44 +0800

    Add permissions for Ingredient model

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,10 +1,12 @@
 class IngredientsController < ApplicationController
   def new
     @ingredient = Ingredient.new
+    authorize @ingredient
   end
 
   def create
     @ingredient = Ingredient.new(ingredient_params)
+    authorize @ingredient
     if @ingredient.save
       redirect_to dashboard_path
     else
@@ -14,6 +16,7 @@ class IngredientsController < ApplicationController
 
   def destroy
     @ingredient = Ingredient.find(params[:id])
+    authorize @ingredient
     @ingredient.destroy
     redirect_to dashboard_path
   end
