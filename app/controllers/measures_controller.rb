1400b68b8d00cc0572ee55cf975573c0fceaa72a
mirhamasala <mirha.masala@me.com>
2020-07-21 18:45:37 +0800

    Add permissions for Measure model

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -2,12 +2,14 @@ class MeasuresController < ApplicationController
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @measure = Measure.new
+    authorize @measure
   end
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @measure = Measure.new(measure_params)
     @measure.recipe = @recipe
+    authorize @measure
     if @measure.save
       redirect_to recipe_path(@recipe)
     else
@@ -17,6 +19,7 @@ class MeasuresController < ApplicationController
 
   def destroy
     @measure = Measure.find(params[:id])
+    authorize @measure
     @measure.destroy
     redirect_to recipe_path(@measure.recipe)
   end
