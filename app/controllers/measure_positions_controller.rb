f223ef37bf13b7ba3efc7f8258a9d7745f35c616
mirhamasala <mirha.masala@me.com>
2020-07-26 08:50:02 +0800

    Fix indentation

--- app/controllers/measure_positions_controller.rb
+++ app/controllers/measure_positions_controller.rb
@@ -2,7 +2,9 @@ class MeasurePositionsController < ApplicationController
   def update
     @measure = Measure.find(params[:measure_id])
     authorize @measure.recipe
+
     @measure.insert_at(params[:position].to_i)
+
     head :ok
   end
 end
