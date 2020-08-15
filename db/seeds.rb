9be2376fa164e687c794f2d2aa8b5fa0be6e170b
mirhamasala <mirha.masala@me.com>
2020-08-15 13:42:24 +0800

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'yaml'
+
+seed_file = Rails.root.join('config', 'locales', 'ingredients.en.yml')
+
+config = YAML.load_file(seed_file)
+
+config['en']['ingredients'].each_value do |ingredient|
+  Ingredient.where(name: ingredient).first_or_create
+end
