09a65a2753e81db86597f78fee5429ed503989e6
mirhamasala <mirha.masala@me.com>
2020-09-05 13:06:38 +0800

    Style the app 🙃

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,33 +1,51 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
   purge: [],
   theme: {
     extend: {
       boxShadow: {
-        "stone-100": "0 0 0 2px #eaeaea",
+        'stone-100': '0 0 0 2px #eaeaea',
       },
       colors: {
         forrest: {
-          "500": "#57696d",
+          '500': '#57696d',
         },
         sand: {
-          "400": "#a19079",
-          "500": "#887760",
+          '400': '#a19079',
+          '500': '#887760',
         },
         sea: {
-          "100": "#b3c7cb",
-          "200": "#869fae",
-          "300": "#738996",
-          "800": "#2d4558",
-          "900": "#1c2b36",
+          '100': '#b3c7cb',
+          '200': '#869fae',
+          '300': '#738996',
+          '400': '#5b6f7b',
+          '800': '#2d4558',
+          '900': '#1c2b36',
         },
         stone: {
-          "100": "#eaeaea",
-          "200": "#909090",
-          "900": "#444444",
+          '100': '#eaeaea',
+          '200': '#909090',
+          '900': '#444444',
         },
+      },
+      flexGrow: {
+        '2': 2,
+      },
+      flexShrink: {
+        '2': 2,
+      },
+      spacing: {
+        '0.5': '.125rem',
+        '2.5': '.625rem',
       },
     },
   },
-  variants: {},
+  variants: {
+    padding: ['responsive', 'first', 'last'],
+    margin: ['responsive', 'first', 'last'],
+
+  },
   plugins: [],
 };
