99ec8cd83a52c8a375dcb52ac2590076743effd0
mirhamasala <mirha.masala@me.com>
2020-09-05 09:05:24 +0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_025319) do
+ActiveRecord::Schema.define(version: 2020_08_22_230850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_22_025319) do
   end
 
   create_table "measures", force: :cascade do |t|
-    t.float "quantity"
+    t.string "quantity"
     t.bigint "unit_id"
     t.bigint "ingredient_id", null: false
     t.bigint "recipe_id", null: false
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2020_08_22_025319) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "fractionable"
   end
 
   create_table "users", force: :cascade do |t|
