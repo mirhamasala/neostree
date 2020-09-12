f7344d9a7e21b61e9cbae11d89cb5a0fde642097
mirhamasala <mirha.masala@me.com>
2020-09-12 10:42:53 +0800

    Fix updating empty measure fields

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -69,8 +69,14 @@ class RecipesController < ApplicationController
                                    steps_attributes: %i[id position description _destroy])
   end
 
+  def empty_measure?(measure_hash)
+    measure_hash['ingredient'].empty? &&
+      measure_hash['quantity'].empty? &&
+      measure_hash['unit_id'].empty?
+  end
+
   def reordered_steps
-    reordered_steps = recipe_params[:steps_attributes].to_unsafe_h
+    reordered_steps = recipe_params[:steps_attributes] ? recipe_params[:steps_attributes].to_unsafe_h : {}
     reordered_steps.each_with_index do |array, index|
       key = array.first
       reordered_steps[key][:position] = (index + 1)
@@ -78,7 +84,14 @@ class RecipesController < ApplicationController
   end
 
   def reordered_measures
+    return {} unless recipe_params[:measures_attributes]
+
     reordered_measures = recipe_params[:measures_attributes].to_unsafe_h
+
+    reordered_measures.delete_if do |_, measure_hash|
+      empty_measure?(measure_hash)
+    end
+
     reordered_measures.each_with_index do |array, index|
       key = array.first
       reordered_measures[key][:position] = (index + 1)
