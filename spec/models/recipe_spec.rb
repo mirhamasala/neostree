aa7278dd20a360c6ef35cf5c7d850ff9220b81bb
mirhamasala <mirha.masala@me.com>
2020-09-26 06:54:48 +0800

    Add recipe specs

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -20,5 +20,15 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'shows user\'s recipes' do
+    user1 = User.create!(first_name: 'User1', username: 'user1', email: 'user1@me.com', password: '123456')
+    user2 = User.create!(first_name: 'User2', username: 'user2', email: 'user2@me.com', password: '123456')
+
+    recipe1 = Recipe.create!(title: 'Avocado Smoothie', user: user1)
+    recipe2 = Recipe.create!(title: 'Coconut Smoothie', user: user1)
+    recipe3 = Recipe.create!(title: 'Strawberry Smoothie', user: user2)
+
+    expect(Recipe.author(user1)).to eq([recipe1, recipe2])
+    expect(Recipe.author(user2)).to eq([recipe3])
+  end
 end
