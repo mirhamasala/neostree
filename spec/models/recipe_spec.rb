e61d668841c4f254c56d10d9a98bab2f49571c6e
mirhamasala <mirha.masala@me.com>
2020-09-26 13:03:58 +0800

    Refactor recipe specs

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -21,12 +21,12 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   it 'shows user\'s recipes' do
-    user1 = User.create!(first_name: 'User1', username: 'user1', email: 'user1@me.com', password: '123456')
-    user2 = User.create!(first_name: 'User2', username: 'user2', email: 'user2@me.com', password: '123456')
+    user1 = create(:user)
+    user2 = create(:user)
 
-    recipe1 = Recipe.create!(title: 'Avocado Smoothie', user: user1)
-    recipe2 = Recipe.create!(title: 'Coconut Smoothie', user: user1)
-    recipe3 = Recipe.create!(title: 'Strawberry Smoothie', user: user2)
+    recipe1 = create(:recipe, user: user1)
+    recipe2 = create(:recipe, user: user1)
+    recipe3 = create(:recipe, user: user2)
 
     expect(Recipe.author(user1)).to eq([recipe1, recipe2])
     expect(Recipe.author(user2)).to eq([recipe3])
