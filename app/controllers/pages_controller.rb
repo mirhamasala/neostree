4b92ddc2a5fe97c8ac5eaa255b4502583af4bdc5
mirhamasala <mirha.masala@me.com>
2020-10-10 17:12:59 +0800

    Add about action to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,12 @@ class PagesController < ApplicationController
   layout 'home'
 
   def home; end
+
+  def about
+    @devon = User.find_by(username: 'devonkan')
+    @mirha = User.find_by(username: 'mirhamasala')
+    @silvia = User.find_by(username: 'silviafalcomer')
+
+    render layout: 'application'
+  end
 end
