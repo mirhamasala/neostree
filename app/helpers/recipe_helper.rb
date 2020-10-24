7b45d8ad064b82b39997ce2cfd7e262bd96da463
mirhamasala <mirha.masala@me.com>
2020-10-24 13:01:55 +0800

    Show recipe images if all the recipes have an image

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -6,4 +6,10 @@ module RecipeHelper
   def human_attribute_status
     Hash[Recipe.statuses.map { |k, _| [k, Recipe.human_attribute_name("status.#{k}")] }]
   end
+
+  def all_with_images_attached?(recipes)
+    recipes.each do |r|
+      return nil if r.photo.attachment.blank?
+    end
+  end
 end
