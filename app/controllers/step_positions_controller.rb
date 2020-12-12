5259c464c16c1dc80179a4c119a5426eb1ff79f5
mirhamasala <mirha.masala@me.com>
2020-12-12 16:04:32 +0800

    Verify scope for step position

--- app/controllers/step_positions_controller.rb
+++ app/controllers/step_positions_controller.rb
@@ -1,6 +1,6 @@
 class StepPositionsController < ApplicationController
   def update
-    @step = Step.find(params[:step_id])
+    @step = policy_scope(Step).find(params[:step_id])
     authorize @step.recipe
 
     @step.insert_at(params[:position].to_i)
