3e8df43694e03c691378883375ec8b30d4d59f91
mirhamasala <mirha.masala@me.com>
2020-12-26 20:37:31 +0800

    Add collections to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,6 @@ class DashboardsController < ApplicationController
     authorize :dashboard
 
     @recipes = policy_scope(current_user.recipes)
+    @collections = policy_scope(current_user.collections)
   end
 end
