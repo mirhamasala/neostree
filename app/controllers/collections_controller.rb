875fc3b2d596fa31597bba97d4fcddbc6b9bbb65
mirhamasala <mirha.masala@me.com>
2021-01-08 06:18:59 +0800

    Revert "Scope recipes in order to alphabetize"
    This reverts commit e309bf6402562916b76711877718c14aa48e371f.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,7 +7,7 @@ class CollectionsController < ApplicationController
     authorize @collection
 
     @user = current_user
-    @recipes = policy_scope(@user.recipes)
+    @recipes = @user.recipes
     @collection.user = @user
   end
 
@@ -47,7 +47,6 @@ class CollectionsController < ApplicationController
 
   def show
     @collection = policy_scope(Collection).find(params[:id])
-    @recipes = policy_scope(@collection.recipes)
     authorize @collection
   end
 
