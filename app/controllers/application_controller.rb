5378b77c327c7a8508c032696cc7c312894bd3d0
Mirha Masala <mirha@hey.com>
2022-02-25 15:31:05 +0100

    Fix test warning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  include Pundit
+  include Pundit::Authorization
 
   after_action :verify_authorized, unless: :skip_pundit?
   after_action :verify_policy_scoped, unless: :skip_pundit?
